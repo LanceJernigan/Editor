2fba6f6f759aea3dca968fb38ca789b7ba41126f
LanceJernigan <lance@knoxweb.com>
2017-05-08 14:15:00 -0400

    Initial Commit

---
+++ config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const context = path.resolve(__dirname, '..');
+const ENTRY_DIR = path.join(context, 'src');
+const BUILD_DIR = path.join(context, 'build');
+const HTML_TEMPLATE = path.join(BUILD_DIR, 'index.template.html');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.join(ENTRY_DIR, '/index.js'),
+    ],
+    output: {
+        path: BUILD_DIR,
+        publicPath: '/',
+        filename: 'assets/[hash].[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        'env',
+                        'es2015',
+                        'react'
+                    ],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlPlugin({
+            template: HTML_TEMPLATE,
+        }),
+    ],
+    devtool: 'source-map',
+}
---
+++ src/components/App/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = props => <h1>Ye ye</h1>
+
+export default App
